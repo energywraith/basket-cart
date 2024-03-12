e2a2c7181cf88137e124774f0538ed8dfe33ee7a
energywraith <energywraith@gmail.com>
2024-03-12 13:35:37 +0100

    Add PaymentCard Component

---
+++ src/components/templates/PaymentCard/PaymentCard.tsx
@@ -0,0 +1,29 @@
+import { Button } from "@/components/common/Button";
+import { Card } from "@/components/common/Card";
+import { paymentMethods } from "@/consts";
+import { useAppMachine } from "@/context/AppMachineContext";
+
+interface PaymentCardProps {
+  onChangeShipping: () => void;
+}
+
+const PaymentCard = ({ onChangeShipping }: PaymentCardProps) => {
+  const { state } = useAppMachine();
+
+  const paymentMethod = state.context.paymentMethod;
+  const paymentMethodLabel = paymentMethods.find(
+    (method) => method.value === paymentMethod
+  )?.label;
+
+  return (
+    <Card className="flex flex-col gap-y-4">
+      <Card.Header as="h2">Payment Details</Card.Header>
+      <h3>{paymentMethodLabel}</h3>
+      <Button variant="text" className="self-start" onClick={onChangeShipping}>
+        Change payment
+      </Button>
+    </Card>
+  );
+};
+
+export { PaymentCard };
